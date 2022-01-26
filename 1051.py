71a31e5b4d8334daf99ca5167b3df95ed7051eed
sw133v <sw133v@gmail.com>
2022-01-26 11:10:53 +0900

    1051_com

--- 1051.py
+++ 1051.py
@@ -1,3 +1,9 @@
+def my_len(list):
+    cnt = 0
+    for i in list:
+        cnt += 1
+    return cnt
+
 def inp_data():
     data = input().split(' ')
     mat = []
@@ -9,15 +15,33 @@ def inp_data():
 
 def my_solve():
     row, col, mmat = inp_data()
-    if row >= col :
-        max = row 
-    else :
-        max = col
+    if row > col :
+        max = col 
+    elif row <= col:
+        max = row
     
-    for i in row:
-        pass
-    pass
+    result = None
+    
+    while True:
+        if result != None:
+            break
+        for r in range(row):
+            if result != None:
+                break
+            if r + max > row:
+                max -= 1
+                break
+            for c in range(col):
+                if c + max > col:
+                    break
+                else :
+                    if mmat[r][c] == mmat[r][c+max-1] and mmat[r][c] == mmat[r+max-1][c] and mmat[r+max-1][c+max-1] == mmat[r+max-1][c]:
+                        result = max
+                        break
+    return result ** 2
+
+print(my_solve())
     #pass
 
 
-inp_data()
+#inp_data()
