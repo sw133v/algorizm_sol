04b3979fb5e9898462864b53571fb23a5417d6b5
sw133v <sw133v@naver.com>
2024-04-30 14:44:06 +0900

    틀린문제 수정

--- [BOJ]2606.py
+++ [BOJ]2606.py
@@ -1,22 +1,31 @@
 import sys
 from collections import defaultdict
 def sol2606():
-    def woram(val):
-        nonlocal computers
-        for i in network[val]:
-            if computers[i]:
-                continue
-            computers[i] = 1
-            woram(i)
 
     n = int(sys.stdin.readline())
-    computers = [0 for i in range(n+1)]
     network = defaultdict(list)
-    for _ in range(int(sys.stdin.readline())):
+    res = set()
+    check_point = [1]
+    computer_link_list = int(sys.stdin.readline())
+    if(computer_link_list == 0):
+        print(0)
+        return
+    
+    for _ in range(computer_link_list):
         p, s = map(int, sys.stdin.readline().split())
         network[p].append(s)
         network[s].append(p)
-    woram(1)
-    print(computers.count(1)-1)
+        
+    while(len(check_point)):
+        temp = check_point.pop(0)
+        for a in network[temp]:
+            if a in res:
+                continue
+            check_point.append(a)
+            res.add(a)
+            
+        if temp in res:
+            continue
+    print(len(res)-1)
 
 sol2606()
