c8480f462d6ea0507c4b21a1271bd26ccb591ad1
sw133v <sw133v@naver.com>
2024-07-10 01:00:58 +0900

    2024.07.10 1문제

--- [BOJ]1012.py
+++ [BOJ]1012.py
@@ -1,43 +1,44 @@
 import sys
-from collections import deque
+from collections import defaultdict
+from copy import deepcopy
 def sol_1012():
     def mainSolAl():
-        def delta(n):
-            x = [0, 1, 0, -1]
-            y = [-1, 0, 1, 0]
+        def delta(coordinate):
+            deltaX = [0, 1, 0, -1]
+            deltaY = [-1, 0, 1, 0]
+            
+            for i in range(4):
+                if (coordinate[0] + deltaX[i]) < 0 or (coordinate[0] + deltaX[i] >= row ):
+                    continue
+                if (coordinate[1] + deltaY[i]) < 0 or (coordinate[1] + deltaY[i] >= col ):
+                    continue
+                
+                if visit[coordinate[0] + deltaX[i]][coordinate[1] + deltaY[i]] and (coordinate[1] + deltaY[i]) in tempDict[coordinate[0] + deltaX[i]]:
+                    visit[coordinate[0] + deltaX[i]][coordinate[1] + deltaY[i]] = 0
+                    delta([coordinate[0] + deltaX[i], coordinate[1] + deltaY[i]])
 
         res = 0
         rowDict = defaultdict(list)
-        # visit = defaultdict(list)
-
         row, col, cabages = map(int, sys.stdin.readline().split())
+        visit = [[1 for _ in range(col)] for _ in range(row)]
+        
         for _ in range(cabages):
             cabageRow, cabageCol = map(int, sys.stdin.readline().split())
             rowDict[cabageRow].append(cabageCol)
-
-        # print(rowDict)
-        for i in rowDict:
-            print(i, rowDict[i])
-
-        # while rowDict:
-        #     tempPoint = -1
-        #     for i in rowDict:
-        #         if rowDict[i]:
-        #             tempPoint = i
-        #             break;
-
             
+        tempDict = deepcopy(rowDict)
 
-        #     res += 1
+        for i in rowDict:
+            for j in rowDict[i]:
+                if (visit[i][j]):
+                    res += 1
+                    visit[i][j] = 0
+                    delta([i, j])
+        print(res)
 
-
-        # print(res)
-
-    # case = map(int, sys.stdin.readline())
     case = int(sys.stdin.readline())
 
     for _ in range(case):
         mainSolAl()
-    # print(case)
 
 sol_1012()
