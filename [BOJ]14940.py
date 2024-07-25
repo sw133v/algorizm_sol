0e34aee91ae24419a7cc6928393e341210e92587
정성우 <sw133v@naver.com>
2024-07-25 10:51:58 +0900

    쉬운최단거리 시간초과 print 갯수를 map을 이용해 줄여봤지만 여전히 시간초과

--- [BOJ]14940.py
+++ [BOJ]14940.py
@@ -1,47 +1,62 @@
 import sys
 from collections import deque
-# sys.setrecursionlimit(1000)
+from pprint import pprint
+from copy import deepcopy
 
 def sol_14940():
-    # def dfs(geo, deep):
-        
-    #     visited[geo[0]][geo[1]] = deep
-    #     for i in range(4):
-    #         nextX = geo[0] + deltaX[i]
-    #         nextY = geo[1] + deltaY[i]
-    #         if nextX < 0 or nextX >= N or nextY < 0 or nextY >= M or visited[nextX][nextY] <= deep + 1:
-    #             continue
-    #         else:
-    #             if map_list[nextX][nextY] == 0:
-    #                 continue
-    #             dfs([nextX, nextY], deep + 1)
-            
-
     deltaX = [0, 1, 0, -1]
     deltaY = [-1, 0, 1, 0]
-    map_list = deque([])
+    map_list = deque()
     N, M = map(int, sys.stdin.readline().strip().split())
-    sys.setrecursionlimit(N*M)
-    visited = [[N*M for _ in range(M)]for _ in range(M)]
-    # startGeo = []
-        for _ in range(M):
+    visited = [[False for _ in range(M)]for _ in range(N)]
+    res = [[0 for _ in range(M)]for _ in range(N)]
+    start_x = None
+    start_y = None
+    task_queue = deque()
+    length = 0
+    
+    for _ in range(N):
         map_list.append(list(map(int, sys.stdin.readline().strip().split())))
     
-    # for i in range(M):
-    #     if 2 in map_list[i]:
-    #         startGeo.append(i)
-    #         for j in range(N):
-    #             if map_list[i][j] == 2:
-    #                 startGeo.append(j)
-    #                 break
+    for i in range(N):
+        for j in range(M):
+            if map_list[i][j] == 2:
+                start_x = i
+                start_y = j
+            if map_list[i][j] == 0:
+                visited[i][j] = True
                 
-    # dfs(startGeo, 0)
-    # for i in range(M):
-    #     visited[i] = [0 if x == N*M else x for x in visited[i]]
+    task_queue.append([[start_x, start_y], ])
+    
+    while task_queue:
+        temp_que = deque()
+        task = task_queue.popleft()
+        for index in task:
+            res[index[0]][index[1]] = length
+            visited[index[0]][index[1]] = True
+            
+            for i in range(4):
+                next_x = index[0] + deltaX[i]
+                next_y = index[1] + deltaY[i]
+                if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+                    continue
+                if visited[next_x][next_y]:
+                    continue
+                if [next_x, next_y] in temp_que:
+                    continue
+                temp_que.append([next_x, next_y])
+        if temp_que != deque():
+            task_queue.append(temp_que)
+        length += 1
 
-    # for xLayer in visited:
-    #     for yLayer in xLayer:
-    #         print(yLayer,end=" ")
-    #     print()
+    for i in range(N):
+        for j in range(M):
+            if visited[i][j]:
+                continue
+            if map_list[i][j]:
+                res[i][j] = -1
+
+    for xLayer in res:
+        print(' '.join(map(str, xLayer)))    
 
 sol_14940()
